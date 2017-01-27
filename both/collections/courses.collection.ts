df13e9e11695ed8ad5cd4a59b857bd1c3042b357
nsbasketball13 <nimishsaxena96@gmail.com>
2017-01-27 00:27:49 -0800

    Upload the Folder: both and Necessary Files

---
+++ both/collections/courses.collection.ts
@@ -0,0 +1,9 @@
+import { MongoObservable } from 'meteor-rxjs';//Wraps Meteor Original API
+
+import { Course } from '../models/course.model';//Imports interface
+/*
+Uses MongoObservable static methods
+Exporting the Course Object from this module to the output.
+*/
+export const Courses = new MongoObservable.Collection<Course>('courses');
+
---
+++ both/models/course.model.ts
@@ -0,0 +1,9 @@
+//Exporting the interface used for the components of each database object.
+export interface Course{
+  units: string;
+  label: string;
+  name: string;
+  importance: string;
+  status: string;
+
+}
