c0f17e4a15a5a169f92c78d371e130da48e19c02
Logan Lasiter <lasiterlogan@gmail.com>
2017-03-15 08:14:04 -0700

    got users working.

---
+++ client/imports/app/login_signup/index.ts
@@ -0,0 +1,7 @@
+import { SignupComponent } from './signup.component';
+import { LoginComponent } from './login.component';
+
+export const LOGIN_SIGNUP_DECLARATIONS = [
+  SignupComponent,
+  LoginComponent
+];
