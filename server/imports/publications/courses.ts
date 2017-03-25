0d2c0e51c119430f4ed861864139d37238a9d37d
David Arce <arroservices@live.com>
2017-03-25 15:16:15 -0700

    first commit

--- server/imports/publications/courses.ts
+++ server/imports/publications/courses.ts
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import { Courses } from '../../both/collections/courses.collection';
+import { Courses } from '../../../both/collections/courses.collection';
 
 Meteor.publish('courses', function() {
   const selector = {
