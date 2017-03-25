c6150131143531b6635d7965878ca26306844549
Logan Lasiter <lasiterlogan@gmail.com>
2017-03-25 14:38:09 -0700

    made assigned courses.

--- both/collections/acourses.collections.ts
+++ both/collections/acourses.collections.ts
@@ -1,8 +1,8 @@
-// import { MongoObservable } from 'meteor-rxjs';//Wraps Meteor Original API
-//
-// import { ACourse } from '../models/acourse.model';//Imports interface
-// /*
-// Uses MongoObservable static methods
-// Exporting the Course Object from this module to the output.
-// */
-// export const ACourses = new MongoObservable.Collection<ACourse>('acourses');
+import { MongoObservable } from 'meteor-rxjs';//Wraps Meteor Original API
+
+import { ACourse } from '../models/acourse.model';//Imports interface
+/*
+Uses MongoObservable static methods
+Exporting the Course Object from this module to the output.
+*/
+export const ACourses = new MongoObservable.Collection<ACourse>('acourses');
--- both/collections/semesters.collection.ts
+++
@@ -1,8 +0,0 @@
-// import { MongoObservable } from 'meteor-rxjs';//Wraps Meteor Original API
-//
-// import { Course } from '../models/semester.model';//Imports interface
-// /*
-// Uses MongoObservable static methods
-// Exporting the Course Object from this module to the output.
-// */
-// export const Courses = new MongoObservable.Collection<Course>('courses');
